908af97def85cad57a9687fa93e853bfdce03bb5
FranRobert <145219647+FranRobert@users.noreply.github.com>
2023-09-16 12:53:18 -0600

    parte2

---
+++ app/src/main/java/com/example/labpmparcial1/views/ListaAmbosActivity.java
@@ -0,0 +1,16 @@
+package com.example.labpmparcial1.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.labpmparcial1.R;
+
+public class ListaAmbosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_ambos);
+    }
+}
--- app/src/main/java/com/example/labpmparcial1/views/MostrarListaActivity.java
+++ app/src/main/java/com/example/labpmparcial1/views/MostrarListaActivity.java
@@ -2,15 +2,53 @@ package com.example.labpmparcial1.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.example.labpmparcial1.R;
 
 public class MostrarListaActivity extends AppCompatActivity {
-
+    private RadioGroup radioGroup;
+    private Button btnContinuar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mostrar_lista);
+        radioGroup = findViewById(R.id.radioGroup);
+        btnContinuar = findViewById(R.id.btnContinuar);
+        btnContinuar.setEnabled(false); // Deshabilitar el botón al inicio
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton selectedRadioButton = findViewById(checkedId);
+                btnContinuar.setEnabled(selectedRadioButton != null);
+            }
+        });
+
+        btnContinuar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int selectedId = radioGroup.getCheckedRadioButtonId();
+                RadioButton selectedRadioButton = findViewById(selectedId);
+
+                String selectedOption = selectedRadioButton.getText().toString();
+
+                if (selectedOption.equals("Libro")) {
+                    Intent intent = new Intent(MostrarListaActivity.this, ListaLibrosActivity.class);
+                    startActivity(intent);
+                } else if (selectedOption.equals("Revista")) {
+                    Intent intent = new Intent(MostrarListaActivity.this, ListaRevistasActivity.class);
+                    startActivity(intent);
+                } else if (selectedOption.equals("Ambos")) {
+                    Intent intent = new Intent(MostrarListaActivity.this, ListaAmbosActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }

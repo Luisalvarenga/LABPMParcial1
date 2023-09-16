4d202b39f2530064e0fa049f56e4333d27846f45
FranRobert <145219647+FranRobert@users.noreply.github.com>
2023-09-16 12:23:22 -0600

    vista roberto

---
+++ app/src/main/java/com/example/labpmparcial1/views/LibroActivity.java
@@ -0,0 +1,16 @@
+package com.example.labpmparcial1.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.labpmparcial1.R;
+
+public class LibroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_libro);
+    }
+}

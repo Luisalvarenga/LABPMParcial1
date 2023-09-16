0fe61c62ab6bb4a786bc600d081096e2c8080b61
FranRobert <145219647+FranRobert@users.noreply.github.com>
2023-09-16 17:30:46 -0600

    ok

--- app/src/main/java/com/example/labpmparcial1/views/ListaLibrosActivity.java
+++ app/src/main/java/com/example/labpmparcial1/views/ListaLibrosActivity.java
@@ -3,8 +3,15 @@ package com.example.labpmparcial1.views;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.labpmparcial1.R;
+import com.example.labpmparcial1.models.Libro;
+import com.example.labpmparcial1.repositories.PublicacionRespository;
+import com.example.labpmparcial1.services.ServiceLocator;
+
+import java.util.List;
 
 public class ListaLibrosActivity extends AppCompatActivity {
 
@@ -12,5 +19,15 @@ public class ListaLibrosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_libros);
+        PublicacionRespository dbSource = ServiceLocator.getInstance().getDBSource();
+        List<Libro> listaLibros = dbSource.getAllListLibros();
+
+        ListView listView = findViewById(R.id.listView);
+
+        // Crear un ArrayAdapter para mostrar la lista de libros en el ListView
+        ArrayAdapter<Libro> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listaLibros);
+
+        // Asignar el ArrayAdapter al ListView
+        listView.setAdapter(adapter);
     }
 }
